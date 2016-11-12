a3e9dccb00669ab518fa8f2fce8946baa73bbb50
Fan Guo <fan.guo@wishabi.com>
2016-11-12 01:45:20 -0500

    lol reset project things idk maybe this will be okay

---
+++ app/src/main/java/me/cfanguo/smarger/BatterySettings.java
@@ -0,0 +1,13 @@
+package me.cfanguo.smarger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BatterySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battery_settings);
+    }
+}
